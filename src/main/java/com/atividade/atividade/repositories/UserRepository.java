1f8d8957b40db30288e42e122d615f352a2e389d
Gleydson Menezes <gsmenezes996@gmail.com>
2024-07-08 18:16:12 -0300

    Service layer, component registration

--- src/main/java/com/atividade/atividade/repositories/UserRepository.java
+++ src/main/java/com/atividade/atividade/repositories/UserRepository.java
@@ -3,6 +3,7 @@ package com.atividade.atividade.repositories;
 import com.atividade.atividade.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface UserRepository extends JpaRepository<User, Long> {
 
 
