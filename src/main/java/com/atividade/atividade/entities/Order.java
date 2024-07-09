579ff9216e454528ff01614c91cbc6024e618cf6
Gleydson Menezes <gsmenezes996@gmail.com>
2024-07-09 14:36:36 -0300

    OrderStatus Enum

--- src/main/java/com/atividade/atividade/entities/Order.java
+++ src/main/java/com/atividade/atividade/entities/Order.java
@@ -1,5 +1,6 @@
 package com.atividade.atividade.entities;
 
+import com.atividade.atividade.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
@@ -19,6 +20,7 @@ public class Order implements Serializable {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
 
+    private Integer orderStatus;
 
     @ManyToOne
     @JoinColumn(name = "client_id")
@@ -26,11 +28,11 @@ public class Order implements Serializable {
 
     public Order(){}
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment,OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
         this.client = client;
-    }
+        setOrderStatus(orderStatus);    }
 
     public Long getId() {
         return id;
@@ -54,6 +56,16 @@ public class Order implements Serializable {
 
     public void setClient(User client) {
         this.client = client;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null) {
+            this.orderStatus = orderStatus.getCode();
+        }
     }
 
     @Override
