1091577c46c2df89e92b056bf1c2ec55c28a4dac
Gleydson Menezes <gsmenezes996@gmail.com>
2024-07-11 16:01:25 -0300

    Exception handling - delete

---
+++ src/main/java/com/atividade/atividade/services/exceptions/DatabaseException.java
@@ -0,0 +1,9 @@
+package com.atividade.atividade.services.exceptions;
+
+public class DatabaseException extends RuntimeException{
+
+    public DatabaseException(String msg){
+        super(msg);
+    }
+
+}
