aac6fc7a8f85ab8990dfaf13f435a4f2066e78ac
Gleydson Menezes <gsmenezes996@gmail.com>
2024-07-11 15:44:57 -0300

    Exception handling - FindById

--- src/main/java/com/atividade/atividade/services/UserService.java
+++ src/main/java/com/atividade/atividade/services/UserService.java
@@ -2,6 +2,7 @@ package com.atividade.atividade.services;
 
 import com.atividade.atividade.entities.User;
 import com.atividade.atividade.repositories.UserRepository;
+import com.atividade.atividade.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,7 +21,7 @@ public class UserService {
 
     public User findById(Long id){
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj){
